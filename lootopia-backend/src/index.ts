5ae10fedf4a126d7cd2e4b2526f30735fce2d132
leilapav <leila.pavilla@supdevinci-edu.fr>
2025-05-30 23:56:05 +0200

    reirer commentaire

--- lootopia-backend/src/index.ts
+++ lootopia-backend/src/index.ts
@@ -9,7 +9,7 @@ const app = new Hono();
 app.use(
   "*",
   cors({
-    origin: "http://localhost:5173", // ou "*" temporairement
+    origin: "http://localhost:5173", 
     allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization"],
   })
